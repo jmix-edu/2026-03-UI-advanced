5ab6cd27a07c18c86dd80e5c231906b754896b08
vaschenkoda <dvaschenko33@gmail.com>
2026-04-02 17:53:37 +0300

    Day 1 content

--- src/main/java/com/company/timesheets/view/client/ClientListView.java
+++ src/main/java/com/company/timesheets/view/client/ClientListView.java
@@ -2,14 +2,54 @@ package com.company.timesheets.view.client;
 
 import com.company.timesheets.entity.Client;
 import com.company.timesheets.view.main.MainView;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.data.renderer.ComponentRenderer;
+import com.vaadin.flow.data.renderer.Renderer;
 import com.vaadin.flow.router.Route;
+import io.jmix.flowui.UiComponents;
 import io.jmix.flowui.view.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Route(value = "clients", layout = MainView.class)
 @ViewController("ts_Client.list")
 @ViewDescriptor("client-list-view.xml")
 @LookupComponent("clientsDataGrid")
+//@PrimaryLookupView(Client.class)
 @DialogMode(width = "64em")
 public class ClientListView extends StandardListView<Client> {
+    @Autowired
+    private UiComponents uiComponents;
+    @Subscribe
+    public void onInit(final InitEvent event) {
+        H2 header = uiComponents.create(H2.class);
+        header.setText("Clients list INIT");
+        getContent().add(header);
+    }
+
+    @Subscribe
+    public void onBeforeShow(final BeforeShowEvent event) {
+        H2 header = uiComponents.create(H2.class);
+        header.setText("Clients list Before Show!");
+        getContent().add(header);
+    }
+
+    @Subscribe
+    public void onReady(final ReadyEvent event) {
+        H2 header = uiComponents.create(H2.class);
+        header.setText("Clients list READY!");
+        getContent().add(header);
+    }
+
+//    @Supply(to = "clientsDataGrid.name", subject = "renderer")
+//    private Renderer<Client> clientsDataGridNameRenderer() {
+//        return new ComponentRenderer<>(client -> {
+//            // TODO: create suitable component
+//            Span span = uiComponents.create(Span.class);
+//            span.setText(client.toString());
+//            return span;
+//        });
+//    }
 
 }
---
+++ src/main/java/com/company/timesheets/view/mytimeentrylist/MyTimeEntryListView.java
@@ -0,0 +1,35 @@
+package com.company.timesheets.view.mytimeentrylist;
+
+
+import com.company.timesheets.app.TimeEntrySupport;
+import com.company.timesheets.entity.TimeEntry;
+import com.company.timesheets.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.component.grid.DataGrid;
+import io.jmix.flowui.kit.action.ActionPerformedEvent;
+import io.jmix.flowui.view.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Route(value = "my-time-entry-list-view", layout = MainView.class)
+@ViewController(id = "ts_TimeEntry.my")
+@ViewDescriptor(path = "my-time-entry-list-view.xml")
+public class MyTimeEntryListView extends StandardView {
+
+    @ViewComponent
+    private DataGrid<TimeEntry> timeEntriesDataGrid;
+    @Autowired
+    private TimeEntrySupport timeEntrySupport;
+
+    @Subscribe("timeEntriesDataGrid.copy")
+    public void onTimeEntriesDataGridCopy(final ActionPerformedEvent event) {
+        TimeEntry selected = timeEntriesDataGrid.getSingleSelectedItem();
+        if (selected == null) {
+            return;
+        }
+
+        TimeEntry copied = timeEntrySupport.copy(selected);
+
+        // todo open detail view
+
+    }
+}
